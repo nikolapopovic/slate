996c25e2d5b50402521e24b43094a7729071a4e8
nikolapopovic <nikola007pop@gmail.com>
2018-12-12 20:03:17 +0100

    changed js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,13 +2,13 @@
 //= require ./app/_search
 
 window.onload = function myFunction() {
-	var display_none = document.getElementById("create-address").nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.style.display = "none";
-	document.getElementById("create-address").nextElementSibling.nextElementSibling.nextElementSibling.addEventListener("click", function(){
-	var x = document.getElementById("create-address").nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
-	if (x.style.display === "none") {
-		x.style.display = "block";
-	} else {
-		x.style.display = "none";
-	}
-})
+		var display_none = document.getElementById("create-address").nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.style.display = "none";
+		document.getElementById("create-address").nextElementSibling.nextElementSibling.nextElementSibling.addEventListener("click", function(){
+		var x = document.getElementById("create-address").nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+	})
 };
